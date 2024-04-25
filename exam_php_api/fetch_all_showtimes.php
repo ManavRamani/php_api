7bf15558ec3f256b476a0fd57d06a99f05b101f2
Mr_Tech(Manav_Ramani) <104958217+ManavRamani@users.noreply.github.com>
2024-04-25 13:03:56 +0530

    Delete exam_php_api directory

--- exam_php_api/fetch_all_showtimes.php
+++
@@ -1,8 +0,0 @@
-<?php
-    require_once 'user_api.php';
-
-    if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-        $users = getAllShowtimes();
-        echo json_encode($users);
-    }
-?>
--- exam_php_api/fetch_all_users.php
+++
@@ -1,8 +0,0 @@
-<?php
-    require_once 'user_api.php';
-
-    if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-        $users = fetchAllUsers();
-        echo json_encode($users);
-    }
-?>
